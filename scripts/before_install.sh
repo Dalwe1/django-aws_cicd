93e5e4e8040f73173caa4f1e99a09759fcbd984a
Dalwe1 <124433873+Dalwe1@users.noreply.github.com>
2023-06-23 15:15:29 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 # clean codedeploy-agent files for a fresh install
-sudo rm -rf /home/ubuntu/install
+sudo rm -rf /home/Amazon Linux /install
 
 # install CodeDeploy agent
-sudo apt-get -y update
-sudo apt-get -y install ruby
-sudo apt-get -y install wget
-cd /home/ubuntu
+sudo yum -y update
+sudo yum -y install ruby
+sudo yum -y install wget
+cd /home/Amazon Linux
 wget https://aws-codedeploy-ap-south-1.s3.amazonaws.com/latest/install
 sudo chmod +x ./install 
 sudo ./install auto
 
 # update os & install python3
-sudo apt-get update
-sudo apt-get install -y python3 python3-dev python3-pip python3-venv
+sudo yum update
+sudo yum install -y python3 python3-dev python3-pip python3-venv
 pip install --user --upgrade virtualenv
 
 # delete app
-sudo rm -rf /home/ubuntu/django-aws_cicd
+sudo rm -rf /home/Amazon Linux/django-aws_cicd
